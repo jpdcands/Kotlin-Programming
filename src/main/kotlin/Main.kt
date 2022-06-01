488903740eb4a9ebaa0c2effc42c21b05b20d81f
Paul <jpdcands211@gmail.com>
2022-06-01 17:08:10 -0400

    Start pg. 47

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERO_NAME)
+    var playerLevel: Int = 4
+    println(playerLevel)
+
+    if (playerLevel == 1) {
+        println("Bubbles in the land of soft things.")
+    } else if (playerLevel <= 5) {
+        println("Save the town from the barbarian invasions.")
+    } else if (playerLevel == 6) {
+        println("Locate the enchanted sword.")
+    } else if (playerLevel == 7) {
+        println("Recover the long-lost artifact of creation.")
+    } else if (playerLevel == 8) {
+        println("Defeat Nogartse, bringer of death and eater of worlds.")
+    } else {
+        println("There are no quests right now.")
+    }
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+}
